023fad2c35771ed1f9c93eb47d0fca3175244e2d
Prudence Wambui <wambuiprudy@gmail.com>
2023-11-28 21:29:16 +0300

    Spaces before comment

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,7 @@
 def uppercase(str):
     for char in str:
         char_code = ord(char)
-        if char_code >= 97 and char_code <= 122:  #check if liwercase
-            char_code -= 32  #convert upper
+        if char_code >= 97 and char_code <= 122:  # check if liwercase
+            char_code -= 32  # convert upper
         print("{:c}".format(char_code), end="")
     print()
