767e268e47845aa2722c29de1f54f2df6b4255c5
Prudence Wambui <wambuiprudy@gmail.com>
2023-12-04 20:25:09 +0300

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,5 +4,5 @@ def no_c(my_string):
     new_string = ""
     for i in my_string:
         if i not in "cC":
-            new_str += i
-    return (new_str)
+            new_string += i
+    return new_string
