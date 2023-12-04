f94754f632a556f694680c906fb03ae0dad8d87d
Prudence Wambui <wambuiprudy@gmail.com>
2023-12-04 20:21:21 +0300

    task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,64 +1,40 @@
 #include "lists.h"
+#include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * is_palindrome - checks if a singly linked list is a palindrome
- * @head: pointer to pointer of the first node of the list
- * Return: 1 if it is a palindrome, 0 otherwise
- */
+  * is_palindrome - A function that checks if a list is a palindrome.
+  * @head: The pointer to the head of the list.
+  * Return: 0 if list not a palindrome, 1 if it is.
+  */
 int is_palindrome(listint_t **head)
 {
-    listint_t *slow, *fast, *prev = NULL, *temp;
-    int is_palindrome = 1;
+	listint_t *tmp = *head;
+	int nodes = 0, i = 0, *array = NULL;
 
-    if (*head == NULL || (*head)->next == NULL)
-        return 1; // An empty list or a list with one element is a palindrome
-
-    slow = *head;
-    fast = *head;
-
-    // Find the middle of the list using slow and fast pointers
-    while (fast != NULL && fast->next != NULL)
-    {
-        fast = fast->next->next;
-
-        // Reverse the first half of the list
-        temp = slow->next;
-        slow->next = prev;
-        prev = slow;
-        slow = temp;
-    }
-
-    // If the list has odd number of elements, move slow to the next node
-    if (fast != NULL)
-        slow = slow->next;
-
-    // Compare the reversed first half with the second half
-    while (prev != NULL && slow != NULL)
-    {
-        if (prev->n != slow->n)
-        {
-            is_palindrome = 0;
-            break;
-        }
-
-        // Move to the next nodes for comparison
-        prev = prev->next;
-        slow = slow->next;
-    }
-
-    // Restore the original list by reversing the first half back
-    while (slow != NULL)
-    {
-        temp = slow->next;
-        slow->next = prev;
-        prev = slow;
-        slow = temp;
-    }
-
-    // Update the head to point to the start of the restored list
-    *head = prev;
-
-    return is_palindrome;
+	if (*head == NULL || head == NULL || (*head)->next == NULL)
+		return (1);
+	while (tmp)
+	{
+		nodes++;
+		tmp = tmp->next;
+	}
+	array = malloc(sizeof(int) * nodes);
+	tmp = *head;
+	while (tmp)
+	{
+		array[i++] = tmp->n;
+		tmp = tmp->next;
+	}
+	for (i = 0; i < nodes / 2; i++)
+	{
+		if (array[i] != array[nodes - 1 - i])
+		{
+			free(array);
+			return (0);
+		}
+	}
+	free(array);
+	return (1);
 }
-
