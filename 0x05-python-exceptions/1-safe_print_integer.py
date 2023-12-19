e7be474a610d30c46c158b0a7959a5021237f58e
Prudence Wambui <wambuiprudy@gmail.com>
2023-12-19 21:44:41 +0300

    correct task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -8,5 +8,6 @@ def safe_print_integer(value):
     """
     try:
         print("{:d}".format(value))
-        return true
-    exceptException:
+        return True
+    except Exception:
+        return False
