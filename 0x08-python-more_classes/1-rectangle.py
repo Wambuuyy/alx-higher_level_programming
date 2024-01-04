42309c9fa7cc9a51dc6b870aa183cd219e5b34e6
Prudence Wambui <wambuiprudy@gmail.com>
2024-01-04 17:59:08 +0300

    task 1 again

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,17 +4,20 @@
 
 
 class Rectangle:
-    """it contains two attributes(private) width and height and they are optional"""
+    """it contains two attributes(private)
+    width and height and they are optional"""
     def  __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
-     @property
+    
+    @property
     def width(self):
         """
         property width to retrieve it
         Returns width(int) of the rectangle
         """
         return self.__width
+    
     @width.setter
     def width(self, value):
         """
@@ -26,6 +29,13 @@ class Rectangle:
         Raises:
         TypeError: If width is not an integer
         ValueError: If width is less than 0
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+    
     @property
     def height(self):
         """
@@ -33,6 +43,7 @@ class Rectangle:
         Returns height(int) of the rectangle
         """
         return self.__height
+    
     @height.setter
     def height(self, value):
         """
@@ -48,10 +59,3 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-        """
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
