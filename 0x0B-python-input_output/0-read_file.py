e8ae99af6d27eabbe9e166c0762c81099837b2db
Prudence Wambui <wambuiprudy@gmail.com>
2024-01-09 11:40:11 +0300

    create readme and task 1

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 0-read_file.py
+# Prudence Wambui
+"""
+a function that reads a textfile and prints it
+"""
+
+def read_file(filename=""):
+    """
+    uses utf-8 encoding
+    input required is filename only
+    so it defaults the mode to read
+    """
+    with open(filename, encoding="UTF-8") as file:
+        content = file.read()
+        print(content)
