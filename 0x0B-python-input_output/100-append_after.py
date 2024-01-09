7778be2aeeab0a3584b9a180cf1894645bd77ecc
Prudence Wambui <wambuiprudy@gmail.com>
2024-01-09 19:58:50 +0300

    task 100

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -14,9 +14,9 @@ def append_after(filename="", search_string="", new_string=""):
     """
     text = ""
     with open(filename) as r:
-    for line in r:
-        text += line
-        if search_string in line:
-            text += new_string
+        for line in r:
+            text += line
+            if search_string in line:
+                text += new_string
     with open(filename, "w") as w:
         w.write(text)
