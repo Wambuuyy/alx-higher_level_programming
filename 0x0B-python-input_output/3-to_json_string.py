42f489cb6a95881c9d3d53d8f4e2b738a8a71ded
Prudence Wambui <wambuiprudy@gmail.com>
2024-01-09 15:21:51 +0300

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# 3-to_json_string.py
+# Prudence Wambui
+"""
+function that returns the JSON 
+representation of an object
+"""
+
+
+def to_json_string(my_obj):
+    """
+    first import json to be able to use it
+    use dumps to encode
+    """
+    import json
+
+    return json.dumps(my_obj)
