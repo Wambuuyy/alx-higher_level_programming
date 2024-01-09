cff4c262499c84699073720d5c605d43e5368c09
Prudence Wambui <wambuiprudy@gmail.com>
2024-01-09 17:05:50 +0300

    task5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# 5-save_to_json_file.py
+#Prudence Wambui
+"""
+function that writes an object to a text file
+using json representation
+"""
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    import json
+    usw w as mode to open file in write only
+    write()
+    dumps to turn to a json string
+    """
+    import json
+
+    with open(filename, 'w') as file:
+        json.dump(my_obj, file)
