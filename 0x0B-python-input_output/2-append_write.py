e781fb2f158348f71545c6ea9d3723240a3a9bd8
Prudence Wambui <wambuiprudy@gmail.com>
2024-01-09 15:12:27 +0300

    task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 2-append_write.py
+#Prudence Wambui
+"""
+function that appends a string at the end of the text file
+returns the number of characters added
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    to append use the'a' mode while opening the file
+    write() returns number of characters written
+    """
+    with open(filename, 'a', encoding='UTF-8') as file:
+        return (file.write(text))
