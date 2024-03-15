0f8aa08bfaa6f73a05c6f97472e35a3fdaefb018
Wambuuyy <wambuiprudy@gmail.com>
2024-03-15 07:27:12 -0700

    add despite duplication

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -16,12 +16,12 @@ if __name__ == "__main__":
                                  user=username, passwd=password, db=database)
     # create cursor to access
     cursor = connection.cursor()
-    query = "
-    SELECT cities.id, cities.name, states.name
-    FROM cities
-    JOIN states ON cities.state_id = states.id
-    ORDER BY cities.id ASC
-    "
+    query = """
+        SELECT cities.id, cities.name, states.name
+        FROM cities
+        INNER JOIN states ON cities.state_id=states.id
+        ORDER BY cities.id ASC
+    """
     cursor.execute(query)
     rows = cursor.fetchall()
     # display results
