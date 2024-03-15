a2334a1ee1bf2a986bc2dc5907bd2b2b72b351ed
Prudence Wambui <wambuiprudy@gmail.com>
2024-03-15 14:26:00 +0300

    script of states starting with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+script that lists states  with a name starting with capital N
+in an ascending order by id
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # the three arguments taken
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    # connect to MySQL server
+    connection = MySQLdb.connect(host="localhost", port=3306,
+                                 user=username, passwd=password, db=database)
+    # create cursor to access
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = cursor.fetchall()
+    # display results
+    for row in rows:
+        print(row)
+    # don forget to close connection
+    cursor.close()
+    connection.close()
