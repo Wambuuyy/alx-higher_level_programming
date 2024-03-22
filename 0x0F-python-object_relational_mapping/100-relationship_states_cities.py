640b94b46351547c76fac5f09e6d7c5e9cb05aea
Prudence Wambui <139547435+Wambuuyy@users.noreply.github.com>
2024-03-22 08:57:28 +0300

    Update 100-relationship_states_cities.py

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -10,6 +10,7 @@ from sqlalchemy.orm import sessionmaker
 from relationship_state import State
 from relationship_city import Base, City
 
+
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
