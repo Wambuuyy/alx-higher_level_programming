9a9056fcb4c88ed67319ce17a364ffdcead39594
Prudence Wambui <139547435+Wambuuyy@users.noreply.github.com>
2024-03-28 19:48:02 +0300

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-"""
-Fetches a URL using urllib and displays information about the response body.
-"""
+"""Fetches https://intranet.hbtn.io/status."""
+import requests
 
-import urllib.request
 
 if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
-
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-
-        print("Body response:")
-        print("\t- type:", type(body))
-        print("\t- content:", body)
-        print("\t- utf8 content:", body.decode('utf-8'))
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
