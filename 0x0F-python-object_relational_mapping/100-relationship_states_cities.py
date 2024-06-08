42889a89105d887f0ea27dd17dc16969f9077a08
Prudence Wambui <139547435+Wambuuyy@users.noreply.github.com>
2024-06-08 16:33:35 +0300

    correct the linking of california and its city

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -23,8 +23,8 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     # create instance of new custom session class
     session = Session()
-    california = State(name="California")
-    california.cities = [City(name="San Francisco")]
-    session.add(california)
+    new_state = State(name="California")
+    new_city = City(name="San Francisco", state=new_state)
+    session.add(new_state)
     session.commit()
     session.close()
